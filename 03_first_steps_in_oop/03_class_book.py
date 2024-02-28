1635bf45b11383f54894ac5dbbe30f28e6de7b87
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-02-28 12:31:12 +0200

    updates

--- 03_first_steps_in_oop/03_class_book.py
+++ 03_first_steps_in_oop/03_class_book.py
@@ -1,12 +1,6 @@
 class Book:
 
     def __init__(self, name, author, pages):
-        self.names = name
+        self.name = name
         self.author = author
         self.pages = pages
-
-
-book = Book("My Book", "Me", 200)
-print(book.names)
-print(book.author)
-print(book.pages)
